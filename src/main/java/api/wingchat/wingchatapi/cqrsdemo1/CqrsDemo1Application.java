ff9b50a6f64152b77cff5c26e84ee84a3bd6a607
cokebear <lgodl1598@itechcompany.kr>
2024-02-05 08:36:49 +0900

    first commit

---
+++ src/main/java/api/wingchat/wingchatapi/cqrsdemo1/CqrsDemo1Application.java
@@ -0,0 +1,13 @@
+package api.wingchat.wingchatapi.cqrsdemo1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CqrsDemo1Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CqrsDemo1Application.class, args);
+  }
+
+}
---
+++ src/test/java/api/wingchat/wingchatapi/cqrsdemo1/CqrsDemo1ApplicationTests.java
@@ -0,0 +1,13 @@
+package api.wingchat.wingchatapi.cqrsdemo1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CqrsDemo1ApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
